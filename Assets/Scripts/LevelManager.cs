fa90cf88fbe50ec0d5fb6134fb18695123966034
SebastianAle <xxsebastianxxale@yahoo.com>
2018-05-28 01:41:22 -0400

    Number Wizard Practice project game

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("Load level requested: " + name);
+		SceneManager.LoadScene (name);
+	}
+
+	public void QuitLevel(){
+		Debug.Log ("I want to quit!");
+	}
+}
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	int max;
+	int min;
+	int guess;
+
+	public Text text;
+
+	public int maxGuessesAllowed = 7;
+	// Use this for initialization
+	void Start () {
+		StartGame ();
+	}
+
+	public void GuessHigher(){
+		min = guess;
+		NextGuess();
+	}
+
+	public void GuessLower(){
+		max = guess;
+		NextGuess ();
+	}
+
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		NextGuess ();
+	}
+
+	void NextGuess (){
+		guess = Random.Range (min, max + 1);
+		text.text = guess.ToString ();
+		maxGuessesAllowed--;
+
+		if (maxGuessesAllowed <= 0) 
+		{
+			SceneManager.LoadScene ("Win");
+		}
+	}
+}
